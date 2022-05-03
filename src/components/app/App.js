48e79753baf79c8cc6bc775f467a9eaedbb98fe4
Vadym-Olefirenko <laimg011@gmail.com>
2022-05-03 22:46:10 +0300

    fix

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,15 @@
+import { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom/cjs/react-router-dom.min";
 import AppHeader from "../appHeader/AppHeader";
 import MainPage from "../pages/MainPage";
 import ComicsPage from "../pages/ComicsPage";
-import Page404 from '../pages/404';
+
 import SingleComicPage from '../pages/SingleComicPage';
 
 import decoration from '../../resources/img/vision.png';
+import Spinner from "../spinner/Spinner";
+
+const Page404 = lazy(() => import('../pages/404'));
 
 
 const App = () => {
@@ -15,21 +19,23 @@ const App = () => {
             <div className="app">
             <AppHeader/>
             <main>
-                <Switch>
-                   <Route path="/" exact>
-                        <MainPage/>
-                   </Route>
-                    <Route path="/comics" exact>
-                        <ComicsPage/>
+                <Suspense fallback={<Spinner/>}>
+                    <Switch>
+                    <Route path="/" exact>
+                            <MainPage/>
                     </Route>
-                    <Route path="/comics/:comicID" exact>
-                        <SingleComicPage/>
-                    </Route>
-                    <Route path="*">
-                        <Page404/>
-                    </Route>
-                    <img className="bg-decoration" src={decoration} alt="vision"/>
-                </Switch>
+                        <Route path="/comics" exact>
+                            <ComicsPage/>
+                        </Route>
+                        <Route path="/comics/:comicID" exact>
+                            <SingleComicPage/>
+                        </Route>
+                        <Route path="*">
+                            <Page404/>
+                        </Route>
+                        <img className="bg-decoration" src={decoration} alt="vision"/>
+                    </Switch>
+                </Suspense>
             </main>
         </div>
         </Router>
